dab7b14eeac87318439f2b528c696ecda7b523d4
shashank011345 <opwjjk@gmail.com>
2026-02-05 09:30:48 +0530

    Fix ALLOWED_HOSTS for Render

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^fznp8%fr-_6+r8(gylsosj-$%i8_1=vs3d0l=h=amoggj01@5'
-
+SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.environ.get("RENDER_EXTERNAL_HOSTNAME")] if os.environ.get("RENDER_EXTERNAL_HOSTNAME") else []
 
 
 # Application definition
