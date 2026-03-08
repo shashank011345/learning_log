250ddfe686c55316cd23e953a835af9e13946ac0
shashank011345 <opwjjk@gmail.com>
2026-03-08 12:05:33 +0530

    Switched back to SQLite in settings.py

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -81,7 +81,12 @@ WSGI_APPLICATION = 'learning_log.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 
-DATABASES = { 'default': dj_database_url.config(default=os.environ.get('DATABASE_URL')) }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
